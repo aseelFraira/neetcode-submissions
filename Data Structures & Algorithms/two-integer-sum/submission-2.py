7c3bc9b21aeaab8f64d3bb9511746df6b0f0d512
aseelFraira <129403290+aseelFraira@users.noreply.github.com>
2026-09-15 16:06:14 +0300

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        freqs = {}
+
+        for i,num in enumerate(nums):
+            missing = target - num
+            if missing in freqs.keys():
+                return [freqs[missing] , i]
+            freqs[num] = i
+        return []
+            
+        
